bb7a0dccdc7d54204687a71c5d223ec473e3d5ab
motomotoyoshi <motomoto.git@gmail.com>
2020-03-07 23:50:24 +0900

    Update, use ejs

--- myapp/app.js
+++ myapp/app.js
@@ -2,15 +2,16 @@ const express = require('express'),
       logger = require('morgan'),
       app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
-app.use((req, res, next) => {
-  console.log('My cunstom middleware !!');
-  next();
-})
 
-app.get('/hello.txt', (req, res) => {
-  res.sendfile(__dirname + '/public/hello.txt');
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Page title'
+  });
 });
 
 app.listen(3000);
