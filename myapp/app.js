2f47e3b580182d8cfcb0f2a04da42c10b047d509
motomotoyoshi <motomoto.git@gmail.com>
2020-03-08 01:16:31 +0900

    Update, use param

--- myapp/app.js
+++ myapp/app.js
@@ -8,14 +8,18 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Page title'
-  });
+app.param('id', (req, res, next, id) => {
+  const users = ['aa', 'bb', 'cc'];
+  req.params.name = users[id];
+  next();
+})
+
+app.get('/hello/:id', (req, res) => {
+  res.send('hello ' + req.params.name);
 });
 
 app.listen(3000);
 console.log(
   'Server starting...\n\n'
   + 'at http://localhost:3000\n'
-  );
+);
