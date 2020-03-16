0d3da8b961bc72f7eaefb3a7c7931af7ff3ada54
motomotoyoshi <motomoto.git@gmail.com>
2020-03-16 22:23:54 +0900

    Update, PUT DELETE

--- blog/app.js
+++ blog/app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(methodOverride())
+app.use(methodOverride('_method'))
 
 app.use(logger('dev'));
 
@@ -20,9 +20,9 @@ app.get('/', post.index);
 app.get('/posts/new', post.new);
 app.post('/posts/create', post.create);
 app.get('/posts/:id([0-9]+)', post.show);
-// app.get('/posts/:id/edit', post.edit);
-// app.put('/posts/:id', post.update);
-// app.delete('/posts/:id', post.destroy);
+app.get('/posts/:id/edit', post.edit);
+app.put('/posts/:id', post.update);
+app.delete('/posts/:id', post.destroy);
 
 app.listen(5000);
 console.log('server starting... http://localhost:5000\n');
--- blog/routes/post.js
+++ blog/routes/post.js
@@ -5,8 +5,34 @@ const posts = [
 ];
 
 exports.index = (req, res) => res.render('posts/index', {posts: posts});
+
 exports.show = (req, res) => res.render('posts/show', {post: posts[req.params.id]});
+
 exports.new = (req, res) => res.render('posts/new');
+
+exports.edit = (req, res) => {
+  res.render(
+    'posts/edit',
+    {
+      post: posts[req.params.id],
+      id: req.params.id
+    }
+  );
+};
+
+exports.update = (req, res) => {
+  posts[req.body.id] = {
+    title: req.body.title,
+    body: req.body.body
+  };
+  res.redirect('/');
+};
+
+exports.destroy = (req, res) => {
+  posts.splice(req.body.id, 1);
+  res.redirect('/');
+};
+
 exports.create = (req, res) => {
   let post = {
     title: req.body.title,
